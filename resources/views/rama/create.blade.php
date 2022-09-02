48b05d5bb431c06b7b013d8fdc6c80c95326b7c6
Juanyonda13 <juan.yonda@misena.edu.com>
2022-09-02 10:03:25 -0500

    create de rama

--- resources/views/rama/create.blade.php
+++ resources/views/rama/create.blade.php
@@ -6,26 +6,19 @@
         <a href="" class="btn bi bi-arrow fs-3"></i></a>
     </div> 
                <div class="col-md-4 col-12 d-flex justify-content-center rounded-5 border p-3 shadow-lg  h-75">
-                       <form action="{{route('caso.store')}}" method="post" class="">
+                       <form action="{{route('rama.store')}}" method="post" class="">
                            @csrf
                            <div class="w-100 justify-content-center d-flex">
                                 <h1>ingresar un caso</h1>
                            </div>
 
                            <div clas s="w-100 justify-content-center d-flex"> 
-                                <label for="exampleFormControlTextarea1" class="form-label">ingrese la descripcion de su caso:</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="descripcion_caso"></textarea>
+                                <label for="exampleFormControlTextarea1" class="form-label">ingrese la rama:</label>
+                                <input class="form-control" id="exampleFormControlTextarea1" rows="3" name="descripcion_caso">
                            </div>
+
                            <div clas s="w-100 justify-content-center d-flex mt-2"> 
-                               <select name="instancia_id" id="" requided class="form-select mt-2">
-                                    <option value="" selected>ingrese su instancia</option>
-                                    @foreach ($instancias as $i)
-                                       <option value="{{$i->id_instancia}}">{{$i->estado}}</option>
-                                    @endforeach
-                               </select>
-                           </div>
-                           <div clas s="w-100 justify-content-center d-flex mt-2"> 
-                               <button class="btn btn-dark w-100 rounded-5 mt-4">Enviar</button>
+                               <button class="btn btn-info w-100 rounded-5 mt-4">Enviar</button>
                            </div>
                            
 
